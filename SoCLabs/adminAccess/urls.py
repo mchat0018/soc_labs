afa3b24dfd28a93b4ad8ac386dbb48ceecae80ce
mchat0018 <mihir19175@iiitd.ac.in>
2022-06-23 16:47:18 +0530

    time and board settings forms included in one page

--- SoCLabs/adminAccess/urls.py
+++ SoCLabs/adminAccess/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
+    path('config/',adminRts,name='adminRts'),
     path('config/board_select/',board_page,name='edit-board'),
     path('config/time_select/', crud, name='edit-time'),
     path("config/delete/<int:pk>/", delete_config, name='delete_config'),
