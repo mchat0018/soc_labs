5448805f4c34242800d7aa12c7c9c9bc3bcc17b8
arpa2001 <arpajitofficial@gmail.com>
2022-07-13 18:29:17 +0530

    Static folder update

--- SoCLabs/SoCLabs/settings.py
+++ SoCLabs/SoCLabs/settings.py
@@ -125,8 +125,11 @@ TIME_INPUT_FORMATS = ('%H:%M',)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
