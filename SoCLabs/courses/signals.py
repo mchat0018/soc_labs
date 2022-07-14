4cf187641f65be085417ef9682d9c253a6c233df
arpa2001 <arpajitofficial@gmail.com>
2022-07-14 18:19:21 +0530

    Trying Signals

--- SoCLabs/courses/signals.py
+++ SoCLabs/courses/signals.py
@@ -1,10 +1,16 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save,post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import *
 
+users = []
 
-# @receiver(pre_save, sender=Course)
-# def delete_slots(sender, instance, **kwargs):
-#     for user in instance.user:
-#         user
+@receiver(pre_save, sender=Course)
+def getUsers(sender, instance, **kwargs):
+    print('ok', instance.students)
+
+@receiver(post_save, sender=Course)
+def updateUsers(sender, instance, **kwargs):
+    print('ok')
+    print(users)
+    print('ok')
